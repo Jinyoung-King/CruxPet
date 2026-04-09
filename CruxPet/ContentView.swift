d938f8c21d9563a9e9ebda7d4bc1cb98f1544a33
jiny.kim <kjy9866@cruxdata.co.kr>
2026-04-09 18:05:48 +0900

    Initial Commit

---
+++ CruxPet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CruxPet
+//
+//  Created by 진영 on 4/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CruxPet/CruxPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CruxPetApp.swift
+//  CruxPet
+//
+//  Created by 진영 on 4/9/26.
+//
+
+import SwiftUI
+
+@main
+struct CruxPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
